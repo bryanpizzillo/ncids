d45bc92e3dcbfe9f5deb3c28059dd1a1324384b6
Derek Lescarbeau <derek.lescarbeau@sapientrazorfish.com>
2022-10-05 15:30:13 -0400

    (#552) Adds nci feature card and nci card group

---
+++ testing/ncids-css-testing/stories/components/nci-feature-card/nci-feature-card.scenarios.js
@@ -0,0 +1,106 @@
+module.exports = [
+	{
+		label: 'nci-feature-card default',
+		storyId: 'components-nci-feature-card--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 639,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 879,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-feature-card feature-card-row',
+		storyId: 'components-nci-feature-card--feature-card-row',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 639,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 879,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-feature-card feature-card-in-text-left',
+		storyId: 'components-nci-feature-card--feature-card-in-text-left',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 639,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 879,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-feature-card feature-card-in-text-right',
+		storyId: 'components-nci-feature-card--feature-card-in-text-right',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 639,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 879,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+];
---
+++ testing/ncids-css-testing/stories/components/nci-feature-card/nci-feature-card.stories.jsx
@@ -0,0 +1,9 @@
+export default {
+	title: 'components/nci-feature-card',
+	argTypes: {},
+};
+
+export * from './default.stories';
+export * from './feature-card-row.stories';
+export * from './feature-card-in-text-right.stories';
+export * from './feature-card-in-text-left.stories';
